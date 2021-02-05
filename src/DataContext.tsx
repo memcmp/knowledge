c90969e47df3484c882fdb9118090a37a39a45d2
Philipp Bogensberger <philipp.bogensberger@gmail.com>
2021-02-05 08:58:51 +0100

    change data structures and what to display

--- src/DataContext.tsx
+++ src/DataContext.tsx
@@ -52,9 +52,15 @@ function getObjects(
 function getSubjects(
   nodes: Nodes,
   node: KnowNode,
-  filter?: Array<NodeType>
+  filter?: Array<NodeType>,
+  filterRelations?: Array<RelationType>
 ): Array<KnowNode> {
   return node.relationsToSubjects
+    .filter(r =>
+      filterRelations === undefined
+        ? true
+        : filterRelations.includes(r.relationType)
+    )
     .map(relation => getNode(nodes, relation.a))
     .filter(n => (filter === undefined ? true : filter.includes(n.nodeType)));
 }
@@ -71,7 +77,11 @@ function useSelectors(): {
     filter?: Array<NodeType>,
     filterRelations?: Array<RelationType>
   ) => Array<KnowNode>;
-  getSubjects: (node: KnowNode, filter?: Array<NodeType>) => Array<KnowNode>;
+  getSubjects: (
+    node: KnowNode,
+    filter?: Array<NodeType>,
+    filterRelations?: Array<RelationType>
+  ) => Array<KnowNode>;
   getAllNodesByType: (nodeType: NodeType) => Array<KnowNode>;
 } {
   const context = React.useContext(RelationContext);
@@ -91,9 +101,10 @@ function useSelectors(): {
     },
     getSubjects: (
       node: KnowNode,
-      filter?: Array<NodeType>
+      filter?: Array<NodeType>,
+      filterRelations?: Array<RelationType>
     ): Array<KnowNode> => {
-      return getSubjects(context.nodes, node, filter);
+      return getSubjects(context.nodes, node, filter, filterRelations);
     },
     getAllNodesByType: (nodeType: NodeType): Array<KnowNode> => {
       return getAllNodesByType(context.nodes, nodeType);
